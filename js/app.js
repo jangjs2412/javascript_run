08591b9f0b8b14f60d860fc5e33d28911484232b
Jang Jae Yong <jangjs2412@gmail.com>
2022-02-10 18:31:24 +0900

    boolean(true,false), null, undefined

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+//1.const and let(var는 현재 별로 쓰이지않는다)
+//const 변하지않는 값, let은 변하는 값
+/*
 const a=5, b=2;
 let myName="JJY";
 
@@ -9,3 +12,13 @@ console.log("My name is " + myName);
 myName="User";
 
 console.log("Your name is "+myName);
+*/
+
+//boolean(true,false), null, undefined(값이 없을 때)
+const amIfat=null;
+let something;
+let true_result=true;
+let false_result=false;
+
+console.log(something ,amIfat);
+console.log(true_result, false_result);
