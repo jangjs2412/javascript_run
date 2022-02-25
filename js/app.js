9dadfa24ab96db3fdab20f879ab59967ef957867
Jang Jae Yong <jangjs2412@gmail.com>
2022-02-25 17:16:32 +0900

    5.function

--- js/app.js
+++ js/app.js
@@ -37,6 +37,7 @@ console.log(daysOfWeek[6]);
 */
 
 //4.object
+/*
 const playerName="jjy";
 const playerNumber=10;
 const playerLevel=85;
@@ -59,3 +60,49 @@ console.log(player.number);
 
 player.lastname="tomato";
 console.log(player);
+*/
+
+//5.function
+//argument(인수):함수에 인수(어떠한 값)를 넣어서 사용할수 있다.
+//function functionName(argument){asdfasdfasdf}
+/*
+function sayHello(userName){
+    console.log("Hello my name is "+userName);
+}
+
+sayHello("jjy");
+sayHello("jang");
+
+function sayHelloAge(userName,age){
+    console.log("Hello my name is "+userName+"("+age+")");
+}
+
+sayHelloAge("asdfasdf",30);
+sayHelloAge("????");    //이 경우는 age값이 없어서 undefined로 출력됨
+
+function plus(a,b){
+    console.log(a+b);
+}
+function minus(a,b){
+    console.log(a-b);
+}
+function multi(a,b){
+    console.log(a*b);
+}
+
+plus(5,3);
+minus(8,4);
+multi(5,3);
+*/
+
+const player={
+    name:"jjy",
+    level:67,
+    job:"gunner",
+    no:123456789,
+    sayHello:function(personName){
+        console.log("Hello "+personName+"! nice to meet you!");
+    }
+}
+
+player.sayHello("jaeyong");
