caf48c884a6ed97a449f0d60e1b5e6916625c25d
Jang Jae Yong <jangjs2412@gmail.com>
2022-03-07 17:52:12 +0900

    event_1

--- js/app.js
+++ js/app.js
@@ -221,3 +221,34 @@ else{
     console.log("You should not drink")
 }
 */
+
+/*
+const title = document.getElementById("title");
+//document.getElementById() : id로 element를 가져온다.
+//element로 함수를 사용하여 값을 가져올수있고 값을 바꿀수 있다.
+title.innerText="got you";
+console.dir(title);
+console.dir(document.body);
+*/
+/*
+//.getElementById() 는 가장 위에(앞에)있는 객체 하나만 가져온다.
+//.getElementsByName() .getElementsByClassName() .getElementsByTagName() 등은 반환값이 배열이다.
+//.querySelector()는 css로 element를 가져온다. 단 가장 위에 하나만 가져오기 때문에 모두 가져오려면 .querySelectorAll()를 쓰자
+const hellos = document.querySelectorAll(".hello h1");
+console.log(hellos);
+
+const test = document.getElementById("title");
+console.log(test);
+*/
+
+const title = document.querySelector(".hello h1");
+
+function handleTitleClick(){
+    title.style.color="blue";
+    console.log("title was clicked!");
+}
+
+title.addEventListener("click", handleTitleClick);
+//title을 click하면 handleTitleClick이라는 function이 동작하길 원함
+//그래서 handleTitleClick 함수에 () 를 안넣은 것이고,
+//즉, js가 대신 실행시켜주길 바라는 것이다.
